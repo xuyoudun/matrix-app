cffee054167017010f515dbc839bdf2b9cb5d4b3
许佑敦 <xysz@cn.ibm.com>
2022-07-29 00:33:48 +0800

    添加antd,  react-router & 首页示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/es/locale/zh_CN';
+import moment, {Moment} from 'moment';
+import 'moment/locale/zh-cn';
+import {Alert, ConfigProvider, DatePicker, message, Table} from 'antd';
+import 'antd/dist/antd.less';
+
 import './App.css';
 
+moment.locale('zh-cn');
+
 function App() {
+  const [date, setDate] = useState<Moment | null>();
+  const handleChange = (value: Moment | null) => {
+    message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
+    setDate(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider componentSize="small" locale={zhCN}>
+      <div style={{width: 400, margin: '100px auto'}}>
+        <DatePicker onChange={handleChange}/>
+        <div style={{marginTop: 16}}>
+          <Alert message="当前日期" description={date ? date.format('YYYY年MM月DD日') : '未选择'}/>
+        </div>
+        <Table/>
+      </div>
+    </ConfigProvider>
   );
 }
 
